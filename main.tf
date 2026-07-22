757a235867424864d95d5c5f34041f12b61a376f
sweda <swedzy20@gmail.com>
2026-07-23 00:21:07 +0100

    Add VPC with public and private subnets

--- main.tf
+++ main.tf
@@ -14,3 +14,32 @@ terraform {
    resource "aws_s3_bucket" "first_bucket" {
      bucket = "your-name-terraform-demo-2026"
    }
+
+resource "aws_vpc" "lab_platform_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "lab-platform-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.lab_platform_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "lab-platform-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.lab_platform_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "lab-platform-private-subnet"
+  }
+}
